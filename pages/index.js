f7923d683cf962cc5920a88658da7b3c6cdd4589
jevrton <jevrton_programmer@outlook.com>
2025-04-03 12:37:07 +0000

    second commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+// definição do nosso "objeto concreto"
+function Home() {
+    return <>
+        <h1>Oi mãe, oi pai</h1>
+        <h3>Essa página é... Simples...</h3>
+        <h3>Mas foi feita através do aprendizado do curso que comprei</h3>
+        <h3>Então, aqui vai uma frase, bíblica, pra vocês :)</h3>
+        <h4>"Deus limpará de seus olhos todas as lágrimas" ~ Apocalipse de João 7:17</h4>
+    </>
+}
+
+// export da função padrão
+export default Home;
