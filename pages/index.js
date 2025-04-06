e97edc04f23398e0e535c3a3158ab504ff735588
jevrton <jevrton_programmer@outlook.com>
2025-04-06 00:56:49 +0000

    alteração de texto greguinho

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,7 @@
 function Home() {
   return (
     <>
-      <h1>Oi mãe, oi pai 👌</h1>
-      <h3>Essa página é... Simples...</h3>
-      <h3>Mas foi feita através do aprendizado do curso que comprei</h3>
-      <h3>Então, aqui vai uma frase, bíblica, pra vocês :)</h3>
-      <h4>
-        "Deus limpará de seus olhos todas as lágrimas" ~ Apocalipse de João 7:17
-      </h4>
+      <h1>Oi greguinho :3</h1>
     </>
   );
 }
